4b587329df445343c8830cef197d5bcc00ca4b64
shivajee98 <ultimateintimater@gmail.com>
2024-07-06 15:39:43 +0530

    dark mode color changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
                     {isDropdownOpen && (
                         <div className="absolute right-0 mt-2 w-48  rounded-md shadow-lg z-10">
                             {menuLinks.map((links) => (
-                                <a key={links.id} href={links.link} className='block px-4 py-2 mb-1 text-white bg-gray-700 hover:bg-orange-500 hover:text-white transition-colors duration-300 rounded-md'>
+                                <a key={links.id} href={links.link} className='block px-4 py-2 mb-1 text-white bg-gray-700 hover:bg-rose-400 hover:text-white transition-colors duration-300 rounded-md'>
                                     {links.title}
                                 </a>
                             ))}
