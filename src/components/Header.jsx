27ae7e6a33a5e3f3f4fcf18f657616701fd99a98
shivajee98 <ultimateintimater@gmail.com>
2024-07-07 02:52:54 +0530

    updates to toggle menu btn when clicked

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Link } from 'react-router-dom'; // Importing Link from React Router
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const [brandName, setBrandName] = useState("Shivajee");
@@ -12,36 +12,38 @@ const Header = () => {
     { title: 'My Blogs', link: '/blogs', id: 6 },
   ]);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const dropdownRef = useRef(null); // Ref to store reference to dropdown menu
+  const dropdownRef = useRef(null);
 
   useEffect(() => {
-    // Function to handle clicks outside of dropdown
     const handleClickOutside = (event) => {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
         setIsDropdownOpen(false);
       }
     };
 
-    // Adding event listener when component mounts
     document.addEventListener('mousedown', handleClickOutside);
 
-    // Cleaning up event listener when component unmounts
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
-  }, []); // Empty dependency array ensures effect runs only once
+  }, []);
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
+  const handleLinkClick = () => {
+    setIsDropdownOpen(false);
+  };
+
   return (
     <div className="mt-4 h-20 items-center main flex justify-between px-4 sm:px-16 text-balance font-bold text-lg">
       <div>
-        <h1 className='text-4xl font-bold hover:text-rose-900'><Link to={menuLinks[0].link}>
-            {brandName.toUpperCase()}</Link></h1>
+        <h1 className='text-4xl font-bold hover:text-rose-900'>
+          <Link to={menuLinks[0].link}>{brandName.toUpperCase()}</Link>
+        </h1>
       </div>
-      <div className="relative" ref={dropdownRef}> {/* Assigning ref to the dropdown container */}
+      <div className="relative" ref={dropdownRef}>
         <button onClick={toggleDropdown} className="text-balance bg-auto hover:bg-auto border-2 border-rose-800 py-2 px-4 rounded-lg shadow-md focus:outline-none">
           Menu
         </button>
@@ -52,6 +54,7 @@ const Header = () => {
                 key={links.id}
                 to={links.link}
                 className='block px-4 py-2 mb-1 text-white bg-gray-700 hover:bg-rose-400 hover:text-white transition-colors duration-300 rounded-md'
+                onClick={handleLinkClick}
               >
                 {links.title}
               </Link>
